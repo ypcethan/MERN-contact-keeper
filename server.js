776b21a84106c292db38f9b8ca9c717b33675b01
EthanChenYen-Peng <ypcethan@gmail.com>
2020-06-17 17:23:35 +0800

    set up basic routes

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+
+const app = express()
+
+const PORT = process.env.PORT || 5000
+
+app.get('/', (req, res) => {
+    res.json({
+        msg: 'Hello'
+    })
+})
+
+app.use('/api/users', require('./routes/users'))
+app.use('/api/contacts', require('./routes/contacts'))
+app.use('/api/auth', require('./routes/auth'))
+
+app.listen(PORT, () => {
+    console.log(
+        `Server started on port ${PORT}`
+    )
+})
